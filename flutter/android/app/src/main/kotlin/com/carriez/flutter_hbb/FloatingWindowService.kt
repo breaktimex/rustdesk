20ddd2ab428689922a279ab9ab064281970ec735
breaktimex <2301302011@qq.com>
2026-07-08 13:27:31 +0800

    Update FloatingWindowService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
@@ -1,6 +1,7 @@
 package com.carriez.flutter_hbb
 
 import android.annotation.SuppressLint
+import com.rmd.remotegame.R
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
